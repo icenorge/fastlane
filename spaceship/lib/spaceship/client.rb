3b350d15474817f1428ed9dcd2ec660374f43f66
Arild Berggren <artdt@yahoo.com>
2023-06-12 15:43:16 +0200

    Add workaround for login issue with apple and enterprise account

--- spaceship/lib/spaceship/client.rb
+++ spaceship/lib/spaceship/client.rb
@@ -533,7 +533,8 @@ module Spaceship
 
           if try_upgrade_2fa_later(response)
             store_cookie
-            fetch_olympus_session
+            # Workaround for issue https://github.com/fastlane/fastlane/issues/21301
+            #fetch_olympus_session
             return true
           end
 
